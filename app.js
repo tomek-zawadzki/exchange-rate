5d77b5fb5aa4a7604968acd16249aff78dcd6cb3
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-11 13:15:07 +0100

    get data from api and implement function to calculating currency

---
+++ app.js
@@ -0,0 +1,28 @@
+const currencyOne = document.querySelector("#currency-one");
+const amountOne = document.querySelector(".amount-one");
+const currencyTwo = document.querySelector("#currency-two");
+const amountTwo = document.querySelector(".amount-two");
+const swapBtn = document.querySelector(".swap");
+const rateInfo = document.querySelector(".rate-info");
+
+const calculate = () => {
+  fetch(
+    `https://api.exchangerate.host/latest?base=${currencyOne.value}&symbols=${currencyTwo.value}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const currency1 = currencyOne.value;
+      const currency2 = currencyTwo.value;
+      console.log(currency1);
+      console.log(currency2);
+
+      const rate = data.rates[currency2];
+      rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(4)} ${currency2}`;
+      amountTwo.value = (amountOne.value * rate).toFixed(2);
+    });
+};
+
+currencyOne.addEventListener("change", calculate);
+currencyTwo.addEventListener("change", calculate);
+
+calculate();
